3627cc5e8190c4804d2c36ed50c23866e026145c
Jámbor Tamás <jambortamasmail@gmail.com>
2018-05-30 10:59:37 +0200

    1. feladat kész

--- feladat_1.java
+++ feladat_1.java
@@ -1,28 +1,25 @@
-public class SzakdogaFeladatok {
-    
-    public static void main(String[] args) {     
-        task12("Ez itt a 9991 osztója, az 1");
+class feladat1 {
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(Integer.parseInt(args[0])));
     }
-    
-    // 1. Írjon olyan függvényt vagy metódust, amely egy természetes számról eldönti, hogy prímszám-e, vagy sem!
+
+    // 1. Írjon olyan függvényt vagy metódust, amely egy természetes számról eldönt
+    // , hogy prímszám-e, vagy sem!
     public static boolean isPrime(int num) {
-	if ((num != 0) && (num != 1)) {
-            for ( int i=2; i<num; i++ ) {
-		if ( (num%i == 0) && ((num != i) && (i != 1)) ) {
+        if ((num != 0) && (num != 1)) {
+            for (int i = 2; i < num; i++) {
+                if ((num % i == 0) && ((num != i) && (i != 1))) {
                     return false;
-		}
+                }
             }
-	}
-        else if(num==0 || num==1) {
+        } else if (num == 0 || num == 1) {
             return false;
-        }
-	else {
+        } else {
             return true;
-	}
-        
+        }
+
         return true;
     }
-    
-    
-}
 
+}
