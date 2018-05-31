843bb83b020130a129ceceacd621a78ef6b22008
FerencziZsolt <ferenczi.zsolt3@gmail.com>
2018-05-31 22:39:18 +0200

    feladat_3 Zsolt verzió

--- feladat_3.java
+++ feladat_3.java
@@ -1,6 +1,28 @@
+import java.util.Random;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 class feladat_3 {
 
     public static void main(String[] args) {
-        System.out.printf("hello");
+        kever(args[0]);
     }
+
+    public static void kever(String szoveg){
+        Random r =new Random();
+        char c[]=new char[szoveg.length()];
+        int a;
+        for(int i=0;i<szoveg.length();i++){
+            c[i]=szoveg.charAt(i);
+        }
+        for(int j=0;j<szoveg.length();j++){
+            a=r.nextInt(szoveg.length());
+            char kar=c[j];
+            c[j]=c[a];
+            c[a]=kar;
+        }
+        for(int k=0;k<szoveg.length();k++){
+            System.out.print(c[k]);
+        }
+    } 
 }
