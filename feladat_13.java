e173fdafab5588491ee2ccb8bb04e08572c799ab
FerencziZsolt <ferenczi.zsolt3@gmail.com>
2018-06-04 19:35:42 +0200

    ez az én verzióm, de teszteld

--- feladat_13.java
+++ feladat_13.java
@@ -1,36 +1,19 @@
-class Feladat_13 {
+class feladat_13 {
 
     public static void main( String[] args ) {
         
-        System.out.println( palindromaE( args[0] ) );
+        System.out.println( isPalindrom( args[0] ) );
 
     }
 
-    static boolean palindromaE( String s ) {
-
-        StringBuilder szo =         new StringBuilder();
-        StringBuilder szoForditva = new StringBuilder();
-
-        for( int i=0; i < s.length(); i++ ) {
-
-            if( s.charAt(i) != ' ' ) {
-
-                szo.append( s.charAt( i ) );
-
+    static boolean isPalindrom( String s ) {
+        int palind=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==s.charAt(s.length()-i-1)){
+                palind++;
             }
-
         }
+        return palind==s.length();
 
-        for( int j = szo.length(); j > 0; j-- ) {
-
-            szoForditva.append( szo.charAt( j - 1 ) );
-
-        }
-
-        System.out.println("szo: " + szo.toString() + " | szoForditva: " + szoForditva.toString());
-
-        return szo.toString() == szoForditva.toString();
-
-    }
-
+}
 }
