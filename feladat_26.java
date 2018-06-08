09ec550e160268ae967b5d2d8cbb71f89bffa52e
Jámbor Tamás <jambortamasmail@gmail.com>
2018-06-08 17:01:10 +0200

    26-os módosítás

--- feladat_26.java
+++ feladat_26.java
@@ -2,7 +2,7 @@ class Feladat_26 {
     
     public static void main(String[] args) {
     
-        int[] arr = {2, 3, 5, 3, 23, 21, 3, 8, 4, 66, 43, 98, 22};
+        int[] arr = {2, 5, 23, 21, 3, 3, 8, 4, 66, 43, 98, 22};
         System.out.println( indexOfNum(arr, 3) );
 
     }
