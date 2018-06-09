e95eab7cb6d2246f281da16dfdbe980153df8bad
Jámbor Tamás <jambortamasmail@gmail.com>
2018-06-09 09:46:26 +0200

    22-es módosítása

--- feladat_22.java
+++ feladat_22.java
@@ -1,50 +1,34 @@
 class Feladat_22 {
 
     public static void main(String[] args) {
-        
-        int[] arr = { 10, 2, 2, 3, 4, 3 };
 
-        System.out.println( vanELeggyakrabbSzam(arr) );
+        int[] arr = { 10, 2, 2, 10, 10, 3, 4, 3 };
+
+        System.out.println(vanELeggyakrabbSzam(arr));
 
     }
 
-    static boolean vanELeggyakrabbSzam( int[] arr ) {
+    static boolean vanELeggyakrabbSzam(int[] arr) {
 
         int maxElofordulas = 0;
         int elofordulas = 0;
+        int legtobbszorEloforduloSzam = 0;
         boolean bool = false;
 
-        for( int i=0; i < arr.length; i++ ) {
-
-            for( int j = i + 1; j < arr.length; j++) {
-
-                if( arr[j] == arr[i] ) {
-
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j])
                     elofordulas++;
-
-                }
-
             }
-
-            if(elofordulas > maxElofordulas) {
-
+            if (elofordulas > maxElofordulas && i != 0 && arr[i] != legtobbszorEloforduloSzam) {
                 maxElofordulas = elofordulas;
-
-            }
-            else if( elofordulas == maxElofordulas ) {
-
-                elofordulas = 0;
-                maxElofordulas = 0;
-
-            }
-            else if( i == arr.length - 1 && elofordulas < maxElofordulas ) {
-
+                legtobbszorEloforduloSzam = arr[i];
                 bool = true;
-
+            } else if (elofordulas == maxElofordulas && arr[i] != legtobbszorEloforduloSzam) {
+                bool = false;
             }
 
             elofordulas = 0;
-
         }
 
         return bool;
